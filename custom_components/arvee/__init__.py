a1edea858c391992b8f3763a334ceb3cb32816ab
Mike Goubeaux <m@mikegoubeaux.com>
2023-07-18 20:30:07 -0700

    innitial

--- custom_components/arvee/__init__.py
+++ custom_components/arvee/__init__.py
@@ -2,7 +2,6 @@ import homeassistant.core as ha
 
 import voluptuous as vol
 
-from timezonefinderL import TimezoneFinder
 from homeassistant import core
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.service import async_register_admin_service
@@ -16,30 +15,6 @@ from .const import (
 )
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-    """Set up the Arvee component."""
-
-    async def async_set_timezone_geo(call: ha.ServiceCall) -> None:
-        """Service handler to set timezone by lat/lng"""
-        tzf = TimezoneFinder()
-        result = tzf.timezone_at(lat=call.data[ATTR_LATITUDE], lng=call.data[ATTR_LONGITUDE])
-
-        if result is None:
-            return
-
-        await hass.config.async_update(
-            time_zone=result
-        )
-
-    async_register_admin_service(
-        hass,
-        DOMAIN,
-        SERVICE_SET_TIMEZONE_GEO,
-        async_set_timezone_geo,
-        vol.Schema({
-            ATTR_LATITUDE: cv.latitude,
-            ATTR_LONGITUDE: cv.longitude
-        })
-    )
 
     async def async_set_timezone(call: ha.ServiceCall) -> None:
         """Service handler to set timezone."""
--- custom_components/arvee/const.py
+++ custom_components/arvee/const.py
@@ -1,6 +1,5 @@
-DOMAIN = "arvee"
+DOMAIN = "timezone_updater"
 SERVICE_SET_TIMEZONE = "set_timezone"
-SERVICE_SET_TIMEZONE_GEO = "set_geo_timezone"
 ATTR_LATITUDE = "latitude"
 ATTR_LONGITUDE = "longitude"
 ATTR_TIMEZONE = "timezone"
